0b4de84ffc419acce17fc120d4da86aff840b0ab
Veronika Bukharina <nika_snow@mail.ru>
2025-11-16 15:03:24 +0500

    Initial project setup and configuration

---
+++ src/main/java/com/education/finalorm/FinalOrmApplication.java
@@ -0,0 +1,13 @@
+package com.education.finalorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalOrmApplication.class, args);
+    }
+
+}
